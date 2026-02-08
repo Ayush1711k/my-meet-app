fb8b2ab736065144732875f0368f186708a65e62
Ayush Sharma <Ayushsharma6221@gmail.com>
2026-02-08 19:08:21 +0530

    Fix path-to-regexp error for Node v22

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidV4 } = require('uuid');
 // This tells Express where to find CSS/JS
 app.use(express.static('public'));
 
-// 1. LANDING PAGE - This must stay at the top
+// 1. LANDING PAGE (Must be first)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/home.html');
 });
@@ -22,9 +22,11 @@ app.get('/left', (req, res) => {
     res.sendFile(__dirname + '/public/left.html');
 });
 
-// 4. THE MEETING ROOM (Strict Regex Route)
-// The ([a-zA-Z0-9-]+) part ensures this ONLY triggers if there is an ID
-app.get('/:room([a-zA-Z0-9-]+)', (req, res) => {
+// 4. THE MEETING ROOM (Clean version for Node v22)
+app.get('/:room', (req, res) => {
+    // If for some reason 'room' is empty (it shouldn't be here), 
+    // it will still serve room.html, but since '/' is handled above,
+    // this will only trigger if there is a value in the URL.
     res.sendFile(__dirname + '/public/room.html');
 });
 
